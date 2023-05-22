42ef87f975e0f35ca2897ef59c518cc7498ff6ee
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-05-22 14:05:03 +0200

    Initial commit.

---
+++ src/keeperapp_assets/src/index.js
@@ -0,0 +1,19 @@
+import { keeperapp } from "../../declarations/keeperapp";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await keeperapp.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
